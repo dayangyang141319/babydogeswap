33aa47427fbff53a2e51582e0fae19ca1d33b803
jindandan <jindd826@163.com>
2024-08-24 16:38:26 +0800

    1

--- node_modules/@ton/ton/dist/wallets/WalletContractV5R1.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './v5r1/WalletContractV5R1';
-export * from './v5r1/WalletV5R1Actions';
-export * from './v5r1/WalletV5R1WalletId';
--- node_modules/tonweb/src/boc/index.js
+++
@@ -1,5 +0,0 @@
-const {BitString} = require("./BitString");
-const {Cell} = require("./Cell");
-const {Slice} = require("./Slice");
-
-module.exports = {BitString, Cell, Slice};
